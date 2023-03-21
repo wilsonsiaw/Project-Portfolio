cffaadca8bd42f1b1e68b7da43ed75246173ab7e
Wilson Siaw <siawwils90@gmail.com>
2023-03-21 13:59:34 +0200

    Add the getComputerChoice function
    The getComputerChoice function let's the computer select
    randomly between Rock, Paper or Scissors to allow gameplay
    between the computer and a user.

---
+++ rock-paper-scissors/script.js
@@ -0,0 +1,30 @@
+// Create an array and assign array the values
+// "Rock", "Paper", "Scissors";
+const array = ["Rock", "Paper", "Scissors"];
+
+// create a function getComputerChoice and pass array
+// a the parameter;
+function getComputerChoice(array) {
+
+// create a variable computeRandomChoice and calculate
+// the random choice;
+    const computeRandomChoice = Math.floor(Math.random() * array.length);
+
+// create a variable randomChoice and assign it array
+// giving computeRandomChoice access to the array;
+    const randomChoice = array[computeRandomChoice];
+
+// return randomChoice;
+    return randomChoice;
+}
+
+// Call the function with array as the argument;
+console.log(getComputerChoice(array));
+
+
+// Create an array: randomArray;
+// Pass randomArray the values rock, paper, scissors;
+// Create a variable: randomChoice;
+// Assign MAth.floor and Math.random to random;
+// Multiply Math.random by the length of randomArray;
+// return randomChoice;
